727cb3098d17bb9cba9e0f0f0f45057fca3e68bf
Paul Heisner <paulheisner@gmail.com>
2024-01-24 14:35:42 +0100

    add new tmdb api functions

--- src/server/utils/tmdb_api.ts
+++ src/server/utils/tmdb_api.ts
@@ -85,7 +85,7 @@ export const fetchMovieByImdbId = async (id: string) => {
 };
 export const backdroppathImagepath = (
   backdrop_path: string,
-  quality: "w300" | "w780" | "w1280" | "w1280" | "original" = "w780"
+  quality: "w300" | "w780" | "w1280" | "original" = "w780"
 ) => {
   return `https://image.tmdb.org/t/p/${quality}/${backdrop_path}` as const;
 };
@@ -94,4 +94,107 @@ export const posterpathImagepath = (
   quality: "w342" | "w500" | "w780" | "original" = "w780"
 ) => {
   return `https://image.tmdb.org/t/p/${quality}/${poster_path}` as const;
+};
+
+const fetchImdbIdSchema = z.object({ imdb_id: z.string() });
+
+const fetchImdbId = async (tmdbId: number) => {
+  const params = new URLSearchParams({
+    api_key: env.TMDB_CLIENT_ID,
+  }).toString();
+  const url = `${env.TMDB_BASE_URL}/${tmdbId}/external_ids?${params}`;
+
+  return fetchImdbIdSchema.parse(await axios.get(url));
+};
+
+const topRatedSiteSchema = z.object({
+  page: z.number(),
+  results: z
+    .object({
+      id: z.number(),
+      title: z.string(),
+      original_title: z.string(),
+      overview: z.string(),
+      popularity: z.number(),
+      poster_path: z.string(),
+      backdrop_path: z.string(),
+      release_date: z.string(),
+    })
+    .array(),
+  total_pages: z.number(),
+});
+
+const fetchTopRatedSite = async (page: number) => {
+  const params = new URLSearchParams({
+    api_key: env.TMDB_CLIENT_ID,
+    page: `${page}`,
+    include_adult: "false",
+    include_video: "false",
+    language: "en-US",
+    sort_by: "vote_average.desc",
+    without_genres: "99,10755",
+    "vote_count.gte": "500",
+  }).toString();
+  const url = `${env.TMDB_BASE_URL}/discover/movie?${params}`;
+  return topRatedSiteSchema.parse((await axios.get(url)).data);
+};
+
+export const fetchMultipleTopRatedSites = async (limit: number) => {
+  let currentPage = 1;
+  const results: TmdbResult[] = [];
+
+  while (currentPage <= limit) {
+    const currentResponse = await fetchTopRatedSite(currentPage);
+    const add = (
+      await Promise.all(
+        currentResponse.results.map((res) => fetchMovieByTmdbId(res.id))
+      )
+    ).filter((res): res is Exclude<typeof res, "error"> => res !== "error");
+    results.push(...add);
+    currentPage++;
+    console.log("currentPage", currentPage);
+  }
+  return results;
+};
+export const fetchNowPlayingMovies = async () => {
+  const params = new URLSearchParams({
+    api_key: env.TMDB_CLIENT_ID,
+    page: "1",
+    include_adult: "false",
+    include_video: "false",
+    language: "en-US",
+    sort_by: "popularity.desc",
+    without_genres: "99,10755",
+    with_release_type: "2|3",
+    "release_date.gte": "{min_date}",
+    "release_date.lte": "{max_date}",
+  }).toString();
+
+  const url = `${env.TMDB_BASE_URL}/discover/movie?${params}`;
+  return topRatedSiteSchema.parse((await axios.get(url)).data);
+};
+
+export const fetchPopularMovies = async () => {
+  const params = new URLSearchParams({
+    api_key: env.TMDB_CLIENT_ID,
+    page: "1",
+    include_adult: "false",
+    include_video: "false",
+    language: "en-US",
+    sort_by: "popularity.desc",
+    without_genres: "99,10755",
+    with_release_type: "2|3",
+    "vote_count.gte": "500",
+  }).toString();
+
+  const url = `${env.TMDB_BASE_URL}/discover/movie?${params}`;
+
+  const data = topRatedSiteSchema.parse((await axios.get(url)).data);
+  console.log(JSON.stringify(data.results));
+  const movies = await Promise.all(
+    data.results.map((movie) => fetchMovieByTmdbId(movie.id))
+  );
+  return movies.filter(
+    (movie): movie is Exclude<typeof movie, "error"> => movie !== "error"
+  );
 };
